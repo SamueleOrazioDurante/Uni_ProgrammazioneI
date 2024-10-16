ca982200c7beecc7ee5aca74c0f8072a2e8b144e
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2024-10-16 11:20:39 +0000

    2 ese pdf (celsius to fah)

---
+++ Lezioni/1_161024/PDF/Es2/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+
+    float gradiCelsius,gradiFahrenheit;
+    
+    printf("Inserisci la temperatura (in celsius): ");
+    scanf("%f",&gradiCelsius);
+
+    gradiFahrenheit = gradiCelsius * 1.8 + 32;
+
+    printf("Gradi: %f °F.\n",gradiFahrenheit);
+    return 0;
+}
