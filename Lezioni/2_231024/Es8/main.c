ab8eab85837302c5c410813760f64489320cf51a
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2024-10-23 11:12:58 +0000

    Es8

---
+++ Lezioni/2_231024/Es8/main.c
@@ -0,0 +1,24 @@
+/*
+Scrivere un programma che, acquisito un valore
+intero da utente, stampi a video 0 se il valore è pari,
+1 se è dispari
+*/
+
+#include <stdio.h>
+
+int main(void){
+
+    int numero;
+            
+    printf("Inserisci un numero qualsiasi (intero es. 1,5,3,... ): ");
+    scanf("%d",&numero);
+
+    if(numero%2==1){
+        printf("Numero dispari\n");
+    }else{
+        printf("Numero pari\n");
+    }
+
+    return 0;
+
+}
