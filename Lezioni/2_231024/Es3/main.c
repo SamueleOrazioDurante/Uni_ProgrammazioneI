02b833ce40127e7974d789cf00248e8912a0a403
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2024-10-23 09:59:11 +0000

    Esercizio 3 pdf

---
+++ Lezioni/2_231024/Es3/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void){
+
+    int minuti,secondi;
+
+    do{
+        printf("Inserisci i minuti: ");
+        scanf("%d",&minuti);
+
+    }while(minuti<0);
+    
+    do{
+        printf("Inserisci i secondi: ");
+        scanf("%d",&secondi);
+
+    }while(secondi<0);
+
+    if(secondi >= 60){
+        minuti += secondi/60;
+        secondi = secondi%60;
+    }
+
+    printf("Durante dell'esperimento di %d minuto/i e %d secondo/i \n",minuti,secondi);
+
+    return 0;
+}
